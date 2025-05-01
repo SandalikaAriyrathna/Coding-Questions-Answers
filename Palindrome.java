2006364a7ce3b81f921f260ca0a93ee9e6448939
B.M.N.D.S.Ariyarathna <88665386+SandalikaAriyrathna@users.noreply.github.com>
2025-05-01 10:44:05 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -2,17 +2,19 @@ public class Palindrome {
     public static void main(String[] args) {
         String a = "abcbc";
         String reverse = "";
-        
-        for (int i = 0; i < a.length(); i++) {   
-             reverse = a.charAt(i) + reverse;  
+    
+        for (int i = a.length() - 1; i >= 0; i--) {
+            reverse += a.charAt(i);
         }
-        System.out.println(reverse);
+    
+        System.out.println(reverse); 
     
         if (a.equals(reverse)) {
-            System.out.println("Palindrome");   
-        } else {
-                System.out.println("Not Palindrome");    
-        }       
-          }
+          System.out.println("Palindrome");
+        }else{
+          System.out.println("Not Palindrome");
+        }
+        
+    }
     
 }
