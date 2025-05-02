d6ee39a258cb644cdbc33951235559963a0c2d55
B.M.N.D.S.Ariyarathna <88665386+SandalikaAriyrathna@users.noreply.github.com>
2025-05-02 14:31:30 +0530

    Create FibonacciSequence.java

---
+++ FibonacciSequence.java
@@ -0,0 +1,15 @@
+public class FibonacciSequence {
+    public static void main(String[] args){
+
+    int a = 0, b = 1;
+    for (int i = 0; i < 10; i++) {
+        System.out.print(a + " ");
+        int sum = a + b;
+        a = b;
+        b = sum;
+    }
+
+    }
+
+
+}
